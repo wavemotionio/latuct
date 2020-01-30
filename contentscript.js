14bc051587c4e9c881c417aa739d6ee438cd2d11
PM Palles <pallespm@upmc.edu>
2020-01-30 18:04:59 -0500

    fix: json stringify flat token

--- contentscript.js
+++ contentscript.js
@@ -21,39 +21,39 @@ button.innerText = "Launch a tab with token"
 button.style = "top:0;right:0;position:absolute;background-color:#333;color:#eee;width:100%;z-index:99999;"
 
 button.onclick = function() {
-	chrome.storage.local.get(['launchurl'], function(a) {
-	    chrome.storage.local.get(['tokenQuerystringParam'], function(b) {
-	    	chrome.storage.local.get(['tokenlocation'], function(c) {
-	    		chrome.storage.local.get(['tokenkey'], function(d) {
-	    			chrome.storage.local.get(['tokendeepvalue'], function(e) {
+    chrome.storage.local.get(['launchurl'], function(a) {
+        chrome.storage.local.get(['tokenQuerystringParam'], function(b) {
+            chrome.storage.local.get(['tokenlocation'], function(c) {
+                chrome.storage.local.get(['tokenkey'], function(d) {
+                    chrome.storage.local.get(['tokendeepvalue'], function(e) {
 
-                        const storedToken = JSON.parse(window[c.tokenlocation][d.tokenkey]);
+                        const storedToken = window[c.tokenlocation][d.tokenkey];
 
                         if (storedToken) {
 
-		    				if (e.tokendeepvalue.length > 0) {
+                            if (e.tokendeepvalue.length > 0) {
 
-                                let tokenValue = deepFind(storedToken, e.tokendeepvalue);
+                                let tokenValue = deepFind(JSON.parse(storedToken), e.tokendeepvalue);
 
-		    					if (tokenValue) {
+                                if (tokenValue) {
                                     let redirectLocation = a.launchurl + '?' + b.tokenQuerystringParam + '=' + tokenValue;
 
-		    						window.open(redirectLocation);
-		    					} else {
-		    						console.log('ERROR: ' + c.tokenlocation + '/' + d.tokenkey + '.' + e.tokendeepvalue + ' not found.');
-		    					}
+                                    window.open(redirectLocation);
+                                } else {
+                                    console.log('ERROR: ' + c.tokenlocation + '/' + d.tokenkey + '.' + e.tokendeepvalue + ' not found.');
+                                }
 
-		    				} else {
-								window.open(a.launchurl + '?' + b.tokenQuerystringParam + '=' + storedToken);
-							}
+                            } else {
+                                window.open(a.launchurl + '?' + b.tokenQuerystringParam + '=' + storedToken);
+                            }
 
-						} else {
-							console.log('ERROR: ' + c.tokenlocation + '/' + d.tokenkey + ' not found.');
-						}
-					});
-				});
-		    });
-	    });
+                        } else {
+                            console.log('ERROR: ' + c.tokenlocation + '/' + d.tokenkey + ' not found.');
+                        }
+                    });
+                });
+            });
+        });
     });
 }
 
